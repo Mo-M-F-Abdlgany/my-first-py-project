31d8205b4c3cd7e9820d74b50305a6cf41ee3f78
Mohamed_Mahmoud._ <57798687+Mo-M-F-Abdlgany@users.noreply.github.com>
2020-11-01 03:08:09 +0200

    Add files via upload

---
+++ Area&Diameter_circle .py
@@ -0,0 +1,10 @@
+#constant
+PI = 3.14
+r=  float(input("Enter the radius of the circle :"))
+#Area_circle 
+area= PI*r**2
+print(area)
+#Diameter_Circle 
+diameter=PI*2*r
+print(diameter)
+#Mohamed_Mahmod's_task
